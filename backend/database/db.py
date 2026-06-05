2f00b6bf47774a06f4b9dfbb5f7ec0f666af0827
yeigen <gabriel.dev.edwards@gmail.com>
2026-06-05 00:50:08 -0500

    build: conexion con la base de datos e __init__.py para exportar los modelos

---
+++ backend/database/db.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass

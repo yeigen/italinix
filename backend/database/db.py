1b5a8e55bd85ae4b0b53b362f480258a9101827b
yeigen <gabriel.dev.edwards@gmail.com>
2026-06-05 16:04:58 -0500

    fix: pequeños arreglemos minimos y comentarios

--- backend/database/db.py
+++ backend/database/db.py
@@ -3,17 +3,17 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv() # cargo el .env
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL") # defino la url del .env
 
-engine = create_async_engine(DATABASE_URL, echo=False)
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DATABASE_URL, echo=False) # defino el motor con sqlalchemy para las conexiones con la db
 
-class Base(DeclarativeBase):
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) # expire_on_commit=False es para persistencia
+
+class Base(DeclarativeBase): # es el modelo base para todas las clases, el padre
     pass
 
-
-async def get_db():
+async def get_db(): # crear una session por request
     async with async_session() as session:
         yield session
--- backend/models/category.py
+++ backend/models/category.py
@@ -11,7 +11,7 @@ class Category(Base):
     __tablename__ = "categories"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100), unique=True)
     description: Mapped[str | None] = mapped_column(Text)
 
     products: Mapped[list[Product]] = relationship(back_populates="category")

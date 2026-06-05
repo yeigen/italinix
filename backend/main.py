dac6bf57c4562835ebcfd6634b89f8b66ca4bbc2
yeigen <gabriel.dev.edwards@gmail.com>
2026-06-05 00:51:32 -0500

    build: main inicial

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from backend!")
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from database.db import engine, Base
+from sqlalchemy import text
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        async with engine.connect() as conn:
+            await conn.execute(text("SELECT 1"))
+            print("Conexión sabrosonga")
+    except Exception as e:
+        print(f"Error conectando la DB {e}")
+        raise
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
